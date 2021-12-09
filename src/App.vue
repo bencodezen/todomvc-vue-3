14aba471a1151e82b1b93130e0ecca59b761330d
Ben Hong <ben@bencodezen.io>
2021-12-09 08:10:15 -0500

    refactor: split app into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,17 @@
 <script>
 import AppFooter from './components/AppFooter.vue'
+import TodoApp from './components/TodoApp.vue'
 
 export default {
   name: 'App',
 
   // Define components that will be used in the template
   components: {
-    AppFooter
+    AppFooter,
+    TodoApp
   },
 
-  // app initial state
-  data: () => ({
-    todos: [],
-    newTodo: '',
-    editedTodo: null,
-    visibility: 'all'
-  }),
-
-  computed: {
-    activeTasks() {
-      return this.todos.filter(todo => !todo.completed)
-    },
-    filteredTodos() {
-      if (this.visibility === 'all') {
-        return this.todos
-      } else if (this.visibility === 'active') {
-        return this.activeTasks
-      } else {
-        return this.todos.filter(todo => todo.completed)
-      }
-    },
-    remaining() {
-      return this.activeTasks.length
-    },
-    allDone: {
-      get() {
-        return this.remaining === 0
-      },
-      set() {
-        this.todos.forEach(async todo => {
-          await this.updateTodo(todo)
-        })
-      }
-    }
-  },
-
-  // methods that implement data logic.
-  // note there's no DOM manipulation here at all.
-  methods: {
-    pluralize(word, count) {
-      return word + (count === 1 ? '' : 's')
-    },
-
-    addTodo() {
-      const value = this.newTodo && this.newTodo.trim()
-      const todoItem = {
-        id: this.todos.length + 1,
-        title: value,
-        completed: false
-      }
-
-      if (!value) {
-        return
-      }
-      this.todos.push(todoItem)
-      this.newTodo = ''
-    },
-
-    updateTodo(todo) {
-      this.todos.find(item => item === todo).completed = !todo.completed
-    },
-
-    removeTodo(todo) {
-      const index = this.todos.indexOf(todo)
-      this.todos.splice(index, 1)
-    },
-
-    editTodo(todo) {
-      this.beforeEditCache = todo.title
-      this.editedTodo = todo
-    },
-
-    doneEdit(todo) {
-      if (!this.editedTodo) {
-        return
-      }
-      this.editedTodo = null
-      todo.title = todo.title.trim()
-      if (!todo.title) {
-        this.removeTodo(todo)
-      }
-    },
-
-    cancelEdit(todo) {
-      this.editedTodo = null
-      todo.title = this.beforeEditCache
-    },
-
-    removeCompleted() {
-      this.todos = this.todos.filter(item => !item.completed)
-    }
-  },
+  // Set the page title when the app is loaded
   created() {
     window.document.title = 'TodoMVC (Vue 3) by @bencodezen'
   }
@@ -108,97 +19,7 @@ export default {
 </script>
 
 <template>
-  <article class="todoapp">
-    <header class="header">
-      <h1>todos</h1>
-      <input
-        class="new-todo"
-        autofocus
-        autocomplete="off"
-        placeholder="What needs to be done?"
-        v-model="newTodo"
-        @keyup.enter="addTodo"
-      />
-    </header>
-    <main class="main" v-show="todos.length">
-      <input
-        id="toggle-all"
-        class="toggle-all"
-        type="checkbox"
-        v-model="allDone"
-      />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        <li
-          class="todo"
-          v-for="todo in filteredTodos"
-          :key="todo.id"
-          :class="{ completed: todo.completed, editing: todo == editedTodo }"
-        >
-          <div class="view">
-            <input
-              class="toggle"
-              type="checkbox"
-              @click="updateTodo(todo)"
-              :checked="todo.completed"
-            />
-            <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
-            <button class="destroy" @click="removeTodo(todo)"></button>
-          </div>
-          <input
-            class="edit"
-            type="text"
-            v-model="todo.title"
-            @blur="doneEdit(todo)"
-            @keyup.enter="doneEdit(todo)"
-            @keyup.esc="cancelEdit(todo)"
-          />
-        </li>
-      </ul>
-    </main>
-    <footer class="footer" v-show="todos.length">
-      <span class="todo-count">
-        <strong v-text="remaining"></strong>
-        {{ pluralize('item', remaining) }} left
-      </span>
-      <ul class="filters">
-        <li>
-          <button
-            @click="visibility = 'all'"
-            :class="{ selected: visibility == 'all' }"
-            class="btn"
-          >
-            All
-          </button>
-        </li>
-        <li>
-          <button
-            @click="visibility = 'active'"
-            :class="{ selected: visibility == 'active' }"
-            class="btn"
-          >
-            Active
-          </button>
-        </li>
-        <li>
-          <button
-            @click="visibility = 'completed'"
-            :class="{ selected: visibility == 'completed' }"
-            class="btn"
-          >
-            Completed
-          </button>
-        </li>
-      </ul>
-      <button
-        class="clear-completed"
-        @click="removeCompleted"
-        v-show="todos.length > remaining"
-      >
-        Clear completed
-      </button>
-    </footer>
-  </article>
+  <TodoApp />
   <AppFooter />
 </template>
 
